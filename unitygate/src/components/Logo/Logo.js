6e21ed8ab93e49c03f5043b7ae036ecb2b6743ed
Bee <ardelmbiplang@gmail.com>
2019-10-11 20:19:23 +0100

    Gosa Logo

--- unitygate/src/components/Logo/Logo.js
+++ unitygate/src/components/Logo/Logo.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import './Logo.css';
+import GosaLogo from './gosalogo.png'
 
 
 
 const Logo = () => {
     return (
       <div className='ma4 mt0'>
-        <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-            <div className="Tilt-inner"> 👽 </div>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner"> 
+              <img src={GosaLogo} alt='gosa logo' ></img>
+            </div>
         </Tilt>
       </div>
     )
